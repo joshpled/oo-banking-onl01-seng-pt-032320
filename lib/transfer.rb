eb8b4722ea84a1790cb4e3fd6c2f6c4a65039793
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-25 18:41:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,14 +13,15 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? && @sender.balance >= @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
+      if self.valid? && @sender.balance >= @amount && @status == "pending"
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete"
+      else
+  		@status = "rejected"
+  		"Transaction rejected. Please check your account balance."
+      end
     end
-@status = "rejected"
-"Transaction rejected. Please check your account balance."
-  end
 
 
 end
